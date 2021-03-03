3f8e599fb808a5537449ada012397cdc4b7ae6f5
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-03-03 22:21:39 +0700

    Created signup page

---
+++ app/src/main/java/com/kikulabs/foodmarketkotlin/ui/auth/signup/SignUpAddressFragment.kt
@@ -0,0 +1,17 @@
+package com.kikulabs.foodmarketkotlin.ui.auth.signup
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.kikulabs.foodmarketkotlin.R
+
+class SignUpAddressFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sign_up_address, container, false)
+    }
+}
