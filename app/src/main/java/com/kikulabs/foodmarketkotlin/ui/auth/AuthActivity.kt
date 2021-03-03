f2737d37a9dda30cc00df28ff7fb17f53f617190
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-03-03 22:45:10 +0700

    Created Auth Activity

---
+++ app/src/main/java/com/kikulabs/foodmarketkotlin/ui/auth/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.kikulabs.foodmarketkotlin.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kikulabs.foodmarketkotlin.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
