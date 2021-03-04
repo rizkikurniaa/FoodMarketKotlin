1faf61fa46251fd435ae15c915169ade5d28df13
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-03-04 20:25:34 +0700

    Called toolbar

--- app/src/main/java/com/kikulabs/foodmarketkotlin/ui/auth/signup/SignUpFragment.kt
+++ app/src/main/java/com/kikulabs/foodmarketkotlin/ui/auth/signup/SignUpFragment.kt
@@ -7,7 +7,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
 import com.kikulabs.foodmarketkotlin.R
+import com.kikulabs.foodmarketkotlin.ui.auth.AuthActivity
 import kotlinx.android.synthetic.main.fragment_sign_up.*
+import kotlinx.android.synthetic.main.layout_toolbar.*
 
 class SignUpFragment : Fragment() {
 
@@ -24,6 +26,7 @@ class SignUpFragment : Fragment() {
             Navigation.findNavController(it)
                 .navigate(R.id.action_sign_up_address, null)
 
+            (activity as AuthActivity).toolbarSignUpAddress()
         }
     }
 }
