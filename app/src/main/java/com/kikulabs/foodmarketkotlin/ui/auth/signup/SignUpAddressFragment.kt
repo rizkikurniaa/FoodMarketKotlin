4d863ec12d81a9341a4935b0a026d16774f65e69
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-03-04 20:14:49 +0700

    Added navigate to success sign up

--- app/src/main/java/com/kikulabs/foodmarketkotlin/ui/auth/signup/SignUpAddressFragment.kt
+++ app/src/main/java/com/kikulabs/foodmarketkotlin/ui/auth/signup/SignUpAddressFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.kikulabs.foodmarketkotlin.R
+import kotlinx.android.synthetic.main.fragment_sign_up_address.*
 
 class SignUpAddressFragment : Fragment() {
 
@@ -13,5 +15,15 @@ class SignUpAddressFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_sign_up_address, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        btn_sign_up_now.setOnClickListener {
+            Navigation.findNavController(it)
+                .navigate(R.id.action_sign_up_success, null)
+
+        }
     }
 }
