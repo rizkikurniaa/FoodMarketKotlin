34d3871786efbf176c0abe073a580fa70c7e1890
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-03-04 20:25:21 +0700

    Customize toolbar

--- app/src/main/java/com/kikulabs/foodmarketkotlin/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/kikulabs/foodmarketkotlin/ui/auth/AuthActivity.kt
@@ -2,9 +2,11 @@ package com.kikulabs.foodmarketkotlin.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.navigation.NavOptions
 import androidx.navigation.Navigation
 import com.kikulabs.foodmarketkotlin.R
+import kotlinx.android.synthetic.main.layout_toolbar.*
 
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +15,7 @@ class AuthActivity : AppCompatActivity() {
 
         val pageRequest = intent.getIntExtra("page_request", 0)
         if (pageRequest == 2) {
+            toolbarSignUp()
             val navOptions = NavOptions.Builder()
                 .setPopUpTo(R.id.fragmentSignIn, true)
                 .build()
@@ -20,5 +23,23 @@ class AuthActivity : AppCompatActivity() {
             Navigation.findNavController(findViewById(R.id.authHostFragment))
                 .navigate(R.id.action_sign_up, null, navOptions)
         }
+    }
+
+    fun toolbarSignUp(){
+        toolbar.title = "Sign Up"
+        toolbar.subtitle = "Register and Eat"
+        toolbar.navigationIcon = resources.getDrawable(R.drawable.ic_arrow_back_000, null)
+        toolbar.setNavigationOnClickListener { onBackPressed() }
+    }
+
+    fun toolbarSignUpAddress(){
+        toolbar.title = "Address"
+        toolbar.subtitle = "Make sure it's valid"
+        toolbar.navigationIcon = resources.getDrawable(R.drawable.ic_arrow_back_000, null)
+        toolbar.setNavigationOnClickListener { onBackPressed() }
+    }
+
+    fun toolbarSignUpSuccess(){
+        toolbar.visibility = View.GONE
     }
 }
