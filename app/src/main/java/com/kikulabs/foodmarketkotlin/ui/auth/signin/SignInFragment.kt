53563e1635f6cc0a5aecd89aad81361eed16b0cf
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-03-04 20:03:44 +0700

    Added intent to sign up

--- app/src/main/java/com/kikulabs/foodmarketkotlin/ui/auth/signin/SignInFragment.kt
+++ app/src/main/java/com/kikulabs/foodmarketkotlin/ui/auth/signin/SignInFragment.kt
@@ -1,11 +1,14 @@
 package com.kikulabs.foodmarketkotlin.ui.auth.signin
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.kikulabs.foodmarketkotlin.R
+import com.kikulabs.foodmarketkotlin.ui.auth.AuthActivity
+import kotlinx.android.synthetic.main.fragment_sign_in.*
 
 class SignInFragment : Fragment() {
 
@@ -13,5 +16,15 @@ class SignInFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_sign_in, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        btn_sign_up.setOnClickListener {
+            val signUp = Intent(activity, AuthActivity::class.java)
+            signUp.putExtra("page_request", 2)
+            startActivity(signUp)
+        }
     }
 }
