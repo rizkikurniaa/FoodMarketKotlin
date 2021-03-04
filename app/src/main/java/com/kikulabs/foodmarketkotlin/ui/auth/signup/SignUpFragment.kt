972d5f5a6d75acd9a672a1d8fcd910f90b26729c
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-03-04 20:12:05 +0700

    Added navigate to address

--- app/src/main/java/com/kikulabs/foodmarketkotlin/ui/auth/signup/SignUpFragment.kt
+++ app/src/main/java/com/kikulabs/foodmarketkotlin/ui/auth/signup/SignUpFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.kikulabs.foodmarketkotlin.R
+import kotlinx.android.synthetic.main.fragment_sign_up.*
 
 class SignUpFragment : Fragment() {
 
@@ -13,5 +15,15 @@ class SignUpFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_sign_up, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        btn_continue.setOnClickListener {
+            Navigation.findNavController(it)
+                .navigate(R.id.action_sign_up_address, null)
+
+        }
     }
 }
