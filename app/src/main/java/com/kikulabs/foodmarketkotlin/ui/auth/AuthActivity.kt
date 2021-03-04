c0de946f0d6f5a10f2382fc3fd17f911553d8b98
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-03-04 20:04:21 +0700

    Updated

--- app/src/main/java/com/kikulabs/foodmarketkotlin/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/kikulabs/foodmarketkotlin/ui/auth/AuthActivity.kt
@@ -2,11 +2,23 @@ package com.kikulabs.foodmarketkotlin.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavOptions
+import androidx.navigation.Navigation
 import com.kikulabs.foodmarketkotlin.R
 
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+
+        val pageRequest = intent.getIntExtra("page_request", 0)
+        if (pageRequest == 2) {
+            val navOptions = NavOptions.Builder()
+                .setPopUpTo(R.id.fragmentSignIn, true)
+                .build()
+
+            Navigation.findNavController(findViewById(R.id.authHostFragment))
+                .navigate(R.id.action_sign_up, null, navOptions)
+        }
     }
 }
